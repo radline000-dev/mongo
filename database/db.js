ee26356db166e8a20dedbe5d6584a5065e5aabe7
KimYoHwan <radline000@gmail.com>
2021-03-09 02:17:11 +0900

    tesisdsa

--- database/db.js
+++ database/db.js
@@ -8,15 +8,9 @@ const connectDB = async () => {
     useFindAndModify: false,
   });
 
-<<<<<<< HEAD
   if (process.env.NODE_ENV === "development") {
     mongoose.set("debug", true);
   }
-=======
-  // if (process.env.NODE_ENV === "development") {
-  //   mongoose.set("debug", true);
-  // }
->>>>>>> 51a808eeb6a152e2c25779633ce9560ab5fadaf5
   console.log(`MongoDB Connected: ${conn.connection.host}`);
 };
 
